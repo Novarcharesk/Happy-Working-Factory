b8383f40d58647e92c1e5b0514905d26009f88de
Nova C - Lab 2 102 <114209659+Novarcharesk@users.noreply.github.com>
2023-07-31 12:53:44 +1000

    Packages

--- Assets/Scripts/ConveyerBelt.cs
+++ Assets/Scripts/ConveyerBelt.cs
@@ -12,7 +12,7 @@ public class ConveyerBelt : MonoBehaviour
             Rigidbody rb = other.GetComponent<Rigidbody>();
 
             // Calculate the velocity of the conveyor belt's surface in world space
-            Vector3 conveyerVelocity = transform.TransformVector(Vector3.forward) * conveyerSpeed;
+            Vector3 conveyerVelocity = transform.TransformVector(Vector3.left) * conveyerSpeed;
 
             // Move the box along with the conveyor belt's surface velocity
             rb.velocity = conveyerVelocity;
