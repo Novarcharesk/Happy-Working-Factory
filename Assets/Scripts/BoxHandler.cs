1e4d124facd249123b94b77e27619e3e0664ccb9
SAEStudentNathanHoward <114543371+SAEStudentNathanHoward@users.noreply.github.com>
2023-08-05 17:00:08 +1000

    Udated game files, edited MonorailController script, started work on the BoxPusher hazard, edited BoxHandler script, edited MonorailSpawner script
    Game Files
    - removed the final bits of the un used files
    
    MonorailController
    - added detection for which box is added to the cargo
    - now moves off the scren and gets destroyed
    - fully functional
    - added code comments
    
    BoxPusher
    - ground work put in to make the box pusher work
    - created the prefab
    
    BoxHandler
    - removed unnecessary code
    - added code comments
    
    MonorailSpawner
    - now functions completely

--- Assets/Scripts/BoxHandler.cs
+++ Assets/Scripts/BoxHandler.cs
@@ -1,59 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BoxHandler : MonoBehaviour
 {
     private MonorailController monorailController;
 
+    // String variable needed to tell who last touched the box
     private string lastTouch;
 
+    private void Start()
+    {
+        monorailController = FindObjectOfType<MonorailController>().GetComponent<MonorailController>();
+    }
+
+    // Runs every frame
     void Update()
     {
+        // Checks to see who touched the box last
         if (lastTouch == "Player1")
         {
+            // Updates the colour of the box to m,atch the player colour
             this.gameObject.GetComponent<MeshRenderer>().material.color = Color.blue;
         }
-        
+
+        // Checks to see who touched the box last
         if (lastTouch == "Player2")
         {
+            // Updates the colour of the box to m,atch the player colour
             this.gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
         }
     }
 
+    // Checks to see if there has been a collision
     private void OnCollisionEnter(Collision collision)
     {
+        // Checks to see if it has collided with a player and then updates the string variable
         if (collision.collider.CompareTag("Player1"))
         {
             lastTouch = "Player1";
-            Debug.Log("Touched by player1");
         }
         
         if (collision.collider.CompareTag("Player2"))
         {
             lastTouch = "Player2";
-            Debug.Log("Touched by player2");
-        }
-        
-        if (collision.collider.CompareTag("Monorail"))
-        {
-            Debug.Log("coliding with monorail");
-            OnMonorailCollision(this.gameObject);
         }
     }
 
-    private void OnMonorailCollision(GameObject monorail)
-    { 
-
+    private void OnMonorailCollision(GameObject box)
+    {
+        MonorailController.cargoLoaded = true;
         monorailController.PlaceBoxOnMonorail(lastTouch);
-        Destroy(this.gameObject);
+        Object.Destroy(box);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Monorail"))
         {
-            OnMonorailCollision(other.gameObject);
+            OnMonorailCollision(gameObject);
         }
     }
 }
--- Assets/Scripts/MonorailSpawner.cs
+++ Assets/Scripts/MonorailSpawner.cs
@@ -6,20 +6,36 @@ using UnityEngine;
 public class MonorailSpawner : MonoBehaviour
 {
     public GameObject monorailPrefab;
-    public float spawnInterval = 5f; // Time interval between monorail spawns
 
+    // bool that tells wether there is a monorail already present
+    public static bool monorailPresent;
+
+    // Declaration of the variables needed to movce the monorail
     private GameObject spawnedMonorail;
     [SerializeField] public GameObject leftMonorailBoundary;
     [SerializeField] public GameObject rightMonorailBoundary;
     [SerializeField] public GameObject midMonorailPosition;
 
+    // Plays at start
     private void Start()
     {
         SpawnMonorail();
     }
 
+    private void Update()
+    {
+        // CHecks to see if there is already a monorail
+        if (monorailPresent == false)
+        {
+            // If there isnt one then spawn a new one
+            SpawnMonorail();
+        }
+    }
+
+    // Method that spawns a new monorail
     private void SpawnMonorail()
     {
+        monorailPresent = true;
         spawnedMonorail = Instantiate(monorailPrefab, new Vector3(leftMonorailBoundary.transform.position.x -10, 0, -11.5f), Quaternion.identity);
     }
 }
